0df32f94b39fa4b29b0cacfbf6ba5ca8701e3940
Piotr Berezowski <piotr.berezowski97@gmail.com>
2020-12-20 21:18:43 +0100

    update gunicorn conf

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,9 @@
 import multiprocessing
+import os
 
-bind = '0.0.0.0:8000'
+port = os.environ.get('PORT', '8000')
+
+bind = f'0.0.0.0:{port}'
 workers = multiprocessing.cpu_count() * 2 + 1
 proc_name = 'user_service'
 max_requests = 0  # num of request after which worker will restart (prevent memory leaks)
